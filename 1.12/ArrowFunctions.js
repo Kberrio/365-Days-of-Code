ed9503444a3d649b35252480b13ff7c6981dc0cc
Kevin Berrio <Kberrio@users.noreply.github.com>
2022-01-16 22:34:45 -0500

    Cleaning up files and updating README file

---
+++ 1.12/ArrowFunctions.js
@@ -0,0 +1,11 @@
+// Named function with multiple parameters
+let sum = (a, b) => a + b
+
+// Named function with one parameter
+let isPositive = number => number >= 0
+
+// Named function with no parameters
+let randomNumber = () => Math.random
+
+// Anonymous function with no parameters
+document.addEventListener('click', () => console.log('Click'))
