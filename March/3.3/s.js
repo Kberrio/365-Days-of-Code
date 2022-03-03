6117d0badf80b42226306ab502511f856b4bc398
kberrio <kberrio@gmail.com>
2022-03-03 12:12:45 -0500

    Update

--- March/3.3/s.js
+++ March/3.3/s.js
@@ -1,13 +1,11 @@
 //Iterate with JavaScript While Loops
-//while loops
 
 // Setup
 const myArray = [];
 
 // Only change code below this line
 let i = 5;
-while (i >= 0 ){
+while (i >= 0) {
 	myArray.push(i);
 	i--;
-
 }
